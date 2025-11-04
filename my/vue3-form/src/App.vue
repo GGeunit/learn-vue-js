2be3d02985923253f89a948666589fc67569d2ef
GGeunit <165870924+GGeunit@users.noreply.github.com>
2025-11-04 17:24:56 +0900

    vue3-form update

---
+++ my/vue3-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form action="">
+      <div>
+        <label for="userId">ID:</label>
+        <input id="userId" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input id="password" type="text" v-model="password">
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
