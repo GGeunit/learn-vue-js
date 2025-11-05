42a43e45d5fe1279b15dfa362808aceb079bafbb
GGeunit <165870924+GGeunit@users.noreply.github.com>
2025-11-05 10:47:32 +0900

    v-for update

--- my/vue3-form/src/App.vue
+++ my/vue3-form/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form action="">
+    <form action="" @submit.prevent="submitForm">
       <div>
         <label for="userId">ID:</label>
         <input id="userId" type="text" v-model="username">
@@ -15,14 +15,61 @@
 </template>
 
 <script>
+  // import 라이브러리이름 from '패키지이름'
+  import axios from 'axios'
+import { ref } from 'vue';
+
   export default {
-    data() {
-      return {
-        username: '',
-        password: ''
+    // Composition API 형태
+    // data
+    setup() {
+      var username = ref('');
+      var password = ref('');
+
+      // methods
+      var submitForm = () => {
+        axios.post('https://jsonplaceholder.typicode.com/users/', {
+          username: username.value,
+          password: password.value
+        }).then(response => {
+          console.log(response)
+        })
       }
-    }
+      return { username, password, submitForm }
+    },
   }
+  // methods: {
+  //   logText() {
+  //     this.password
+  //   }
+  // }
+    
+
+    // Options API 형태
+    // data() {
+    //   return {
+    //     username: '',
+    //     password: ''
+    //   }
+    // },
+    // methods: {
+    //   submitForm() {
+    //     // event.preventDefault();
+    //     const data = {
+    //       username: this.username,
+    //       password: this.password
+    //     }
+    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
+    //       .then(response => {
+    //         console.log(response)
+    //       })
+
+    //     console.log('제출됨')
+    //     this.username = '',
+    //     this.password = ''
+    //   }
+    // }
+  // }
 </script>
 
 <style scoped>
